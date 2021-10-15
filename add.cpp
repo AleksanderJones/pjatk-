8ce45c20385d4a58ddf0cc04857929831785ed11
AleksanderJones <91730878+AleksanderJones@users.noreply.github.com>
2021-10-15 12:48:59 +0200

    Add files via upload

---
+++ add.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+ std::cout << "Podaj liczbe a: ";
+ int a{};
+ std::cin >> a;
+ std::cout << "Podaj liczbe b: ";
+int b{};
+ std::cin >> b;
+ int result = a + b;
+ std::cout << result << '\n';
+
+}
---
+++ name.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+ std::cout << "Podaj swoje imie ";
+ int x{};
+ std::cin >> x; 
+ std::cout << "Hello Aleksander";
+ return 0;
+
+}
